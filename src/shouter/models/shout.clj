1fadec8367d11ef5f1e683ca1f7b227a8b47a2c5
abelia <inturi99@gmail.com>
2015-09-26 10:12:59 +0530

    Intial Commit

---
+++ src/shouter/models/shout.clj
@@ -0,0 +1,20 @@
+(ns shouter.models.shout
+  (:require [clojure.java.jdbc :as sql]))
+(let [db-host "localhost"
+      db-port 5432
+      db-name "shouter"]
+
+  (def spec {:classname "org.postgresql.Driver" ; must be in classpath
+           :subprotocol "postgresql"
+           :subname (str "//" db-host ":" db-port "/" db-name)
+                                        ; Any additional keys are passed to the driver
+                                        ; as driver-specific properties.
+           :user "postgres"
+           :password "Design_20"}))
+
+(defn all []
+  (into [] (sql/query spec ["select * from shouts order by id desc"])))
+
+(defn create [shout]
+  (sql/insert! spec :shouts [:body] [shout]))
+
