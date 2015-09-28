a767f8d83b99f0d350e3f012ef4a0f431a7510f5
abelia <inturi99@gmail.com>
2015-09-28 12:11:19 +0530

    Add Procfile

--- src/shouter/models/shout.clj
+++ src/shouter/models/shout.clj
@@ -1,23 +1,24 @@
 (ns shouter.models.shout
   (:require [clojure.java.jdbc :as sql]))
 
-;; (def spec (or (System/getenv "DATABASE_URL")
-;;              "postgresql://localhost:5432/shouter"))
+ (def spec (or (System/getenv "DATABASE_URL")
+              "postgresql://localhost:5432/shouter?user=postgres&password=Design_20&ssl=true"))
 
-(let [db-host "localhost"
-      db-port 5432
-      db-name "shouter"]
-  (def spec {:classname "org.postgresql.Driver" ; must be in classpath
-           :subprotocol "postgresql"
-           :subname (str "//" db-host ":" db-port "/" db-name)
-                                        ; Any additional keys are passed to the driver
-                                        ; as driver-specific properties.
-           :user "postgres"
-             :password "Design_20"}))
+;; (let [db-host "localhost"
+;;       db-port 5432
+;;       db-name "shouter"]
+;;   (def spec {:classname "org.postgresql.Driver" ; must be in classpath
+;;            :subprotocol "postgresql"
+;;            :subname (str "//" db-host ":" db-port "/" db-name)
+;;                                         ; Any additional keys are passed to the driver
+;;                                         ; as driver-specific properties.
+;;            :user "postgres"
+;;              :password "Design_20"}))
 
 (defn all []
   (into [] (sql/query spec ["select * from shouts order by id desc"])))
 
 (defn create [shout]
   (sql/insert! spec :shouts [:body] [shout]))
+
 
